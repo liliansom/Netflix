f2f83f0f353691c677eb96e674e64b9e5debe369
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-03-26 23:41:29 -0300

    Realizadas novas querys no SQL

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
+-- Analisando valores por títulos
+SELECT *
+FROM netflix
+ORDER BY director
+
+
+-- Realizando filtro de categorias
 SELECT TRIM(value) AS listed_in_item, COUNT(*) AS count
 FROM netflix
 CROSS APPLY STRING_SPLIT(listed_in, ',')
@@ -5,9 +12,7 @@ GROUP BY TRIM(value)
 ORDER BY count DESC
 
 
-SELECT *
-FROM netflix
-
+-- Realizando filtro de diretores
 SELECT TRIM(value) AS directors_item, COUNT(*) AS count
 FROM netflix
 CROSS APPLY STRING_SPLIT(director, ',')
@@ -15,8 +20,40 @@ GROUP BY TRIM(value)
 ORDER BY count DESC
 
 
+-- Realizando filtro de países
 SELECT TRIM(value) AS directors_item, COUNT(*) AS count
 FROM netflix
 CROSS APPLY STRING_SPLIT(country, ',')
 GROUP BY TRIM(value)
 ORDER BY count DESC
+
+
+-- Realizando filtro de países
+SELECT TRIM(value) AS cast, COUNT(*) AS count
+FROM netflix
+CROSS APPLY STRING_SPLIT(cast, ',')
+GROUP BY TRIM(value)
+ORDER BY count DESC
+
+
+-- Tratando títulos 
+UPDATE netflix
+SET title = REPLACE(title, '?', '');
+
+UPDATE netflix
+SET title = REPLACE(title, '#', '');
+
+
+-- Removendo títulos vazios
+DELETE FROM netflix WHERE NULLIF(title, ':') IS NULL;
+
+
+-- Deletando filme com nome " : "
+DELETE FROM netflix
+WHERE show_id = (
+  SELECT TOP 1 show_id
+  FROM netflix
+  ORDER BY title
+)
+
+
